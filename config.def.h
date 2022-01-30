ce6c5c4abdfbf2b279b83209186a5455bd51c0d4
hoaxdream <hoaxdream@gmail.com>
2022-01-30 20:12:10 +0800

    Adjust gap size

--- config.def.h
+++ config.def.h
@@ -4,9 +4,9 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 0;        /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
 static const int colorfultag        = 1;        /* 0 means use SchemeSel for selected tag */
 static const int showbar            = 1;        /* 0 means no bar */
