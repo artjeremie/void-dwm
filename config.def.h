a53a9426e2a5803071ddbc7889fc98cf9545567f
hoaxdream <hoaxdream@gmail.com>
2022-03-09 14:42:06 +0800

    statuscmd with status2d

--- config.def.h
+++ config.def.h
@@ -88,6 +88,8 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+#define STATUSBAR "dwmblocks"
+
 #include <X11/XF86keysym.h>
 
 /* commands */
@@ -194,17 +196,19 @@ static Key keys[] = {
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-    /* click                event mask      button          function        argument */
-    { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-    { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-    { ClkWinTitle,          0,              Button2,        zoom,           {0} },
-    { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-    { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-    { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-    { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-    { ClkTagBar,            0,              Button1,        view,           {0} },
-    { ClkTagBar,            0,              Button3,        toggleview,     {0} },
-    { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-    { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+    /* click            event mask  button      function        argument */
+    { ClkLtSymbol,      0,          Button1,    setlayout,      {0} },
+    { ClkLtSymbol,      0,          Button3,    setlayout,      {.v = &layouts[2]} },
+    { ClkWinTitle,      0,          Button2,    zoom,           {0} },
+    { ClkStatusText,    0,          Button1,    sigstatusbar,   {.i = 1} },
+    { ClkStatusText,    0,          Button2,    sigstatusbar,   {.i = 2} },
+    { ClkStatusText,    0,          Button3,    sigstatusbar,   {.i = 3} },
+    { ClkClientWin,     MODKEY,     Button1,    movemouse,      {0} },
+    { ClkClientWin,     MODKEY,     Button2,    togglefloating, {0} },
+    { ClkClientWin,     MODKEY,     Button3,    resizemouse,    {0} },
+    { ClkTagBar,        0,          Button1,    view,           {0} },
+    { ClkTagBar,        0,          Button3,    toggleview,     {0} },
+    { ClkTagBar,        MODKEY,     Button1,    tag,            {0} },
+    { ClkTagBar,        MODKEY,     Button3,    toggletag,      {0} },
 };
 
